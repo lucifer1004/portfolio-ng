803d65192dcd558074646b39edee5198a10c602a
Gabriel Wu <qqbbnease1004@126.com>
2019-01-22 07:39:10 +0800

    feat: basic style setting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BlogComponent } from './blog/blog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/blog/blog.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-blog',
+//   template: `<h2>
+//   blog works!
+// </h2>`,
+  templateUrl: './blog.component.html',
+  styleUrls: ['./blog.component.styl']
+})
+export class BlogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
